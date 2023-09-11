44d6d5e4d8550fe9accbb73a347a0a24a76cfe94
Malachi <ukomalachi@gmail.com>
2023-09-11 23:28:10 +0100

    Use cloud database.

--- server/index.js
+++ server/index.js
@@ -62,7 +62,7 @@ app.use('/posts', postRoutes)
 
 //MONGOOSE SETUP
 const PORT = process.env.PORT || 6001
-mongoose.connect(process.env.MONGO_URL_DEV)
+mongoose.connect(process.env.MONGO_URL)
     .then(async () => {
         // await User.deleteMany()
         // await Post.deleteMany()
